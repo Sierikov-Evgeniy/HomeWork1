3ca5b8217f02fbe9091f9f22f2ec73f8ca49d4ac
Sierikov-Evgeniy <sierikov25@gmail.com>
2021-02-01 15:47:14 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let func = function(){
+    let arr = [1,2,3,4,5,6,7,8,9,10];
+console.log(arr);
+}
+
+func();
+
+let b = function(){
+    let ar = [2,3,4,5,6,7,8,9];
+    ar.push(10);
+    ar.unshift(1);
+    console.log(ar);
+}
+
+b();
+
+
+let a = function(){
+    for(let array = [1] ; array.length < 11 ; array.length++)
+    console.log(array );
+}
+
+a();
+
+
+
+
