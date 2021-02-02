4d16911e1097c70e7fb572454155990c26ebc396
Sierikov-Evgeniy <sierikov25@gmail.com>
2021-02-02 12:33:39 +0300

    fix

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-let func = function(){
-    let arr = [1,2,3,4,5,6,7,8,9,10];
-console.log(arr);
+let array = [1,2,3,4,5,6,7,8,9,10];
+
+let makeEvenNumbers =(argument)=>{
+let finalArr=[];
+
+    for(let i = 0 ; i < argument.length ; i++){
+
+        if((argument[i] %2) === 0){
+            finalArr.push(argument[i])
+        }
+    }
+    return finalArr;
 }
 
-func();
+const bigFUckenDick= makeEvenNumbers(array);
 
-let b = function(){
-    let ar = [2,3,4,5,6,7,8,9];
-    ar.push(10);
-    ar.unshift(1);
-    console.log(ar);
-}
-
-b();
-
-
-let a = function(){
-    for(let array = [1] ; array.length < 11 ; array.length++)
-    console.log(array );
-}
-
-a();
-
+ 
 
 
 
